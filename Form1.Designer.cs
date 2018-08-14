5d898e3ac6e76aa1ed8eb72d8da23527ff1ca7a1
prasad1261997 <35391887+prasad1261997@users.noreply.github.com>
2018-08-14 17:45:27 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MindGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartGame = new System.Windows.Forms.Button();
+            this.MoveLabel = new System.Windows.Forms.Label();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.CurrentSelectedLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.EndGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartGame
+            // 
+            this.StartGame.Location = new System.Drawing.Point(28, 37);
+            this.StartGame.Name = "StartGame";
+            this.StartGame.Size = new System.Drawing.Size(75, 23);
+            this.StartGame.TabIndex = 0;
+            this.StartGame.Text = "StartGame";
+            this.StartGame.UseVisualStyleBackColor = true;
+            this.StartGame.Click += new System.EventHandler(this.StartGame_Click);
+            // 
+            // MoveLabel
+            // 
+            this.MoveLabel.AutoSize = true;
+            this.MoveLabel.Location = new System.Drawing.Point(341, 37);
+            this.MoveLabel.Name = "MoveLabel";
+            this.MoveLabel.Size = new System.Drawing.Size(39, 13);
+            this.MoveLabel.TabIndex = 2;
+            this.MoveLabel.Text = "Moves";
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Location = new System.Drawing.Point(396, 37);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(61, 13);
+            this.ScoreLabel.TabIndex = 3;
+            this.ScoreLabel.Text = "ScoreLabel";
+            // 
+            // CurrentSelectedLabel
+            // 
+            this.CurrentSelectedLabel.AutoSize = true;
+            this.CurrentSelectedLabel.Location = new System.Drawing.Point(344, 89);
+            this.CurrentSelectedLabel.Name = "CurrentSelectedLabel";
+            this.CurrentSelectedLabel.Size = new System.Drawing.Size(89, 13);
+            this.CurrentSelectedLabel.TabIndex = 4;
+            this.CurrentSelectedLabel.Text = "Current Selected:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 43.42857F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.57143F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 126F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 105F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(28, 144);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 64F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(405, 313);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // EndGame
+            // 
+            this.EndGame.Location = new System.Drawing.Point(168, 36);
+            this.EndGame.Name = "EndGame";
+            this.EndGame.Size = new System.Drawing.Size(75, 23);
+            this.EndGame.TabIndex = 6;
+            this.EndGame.Text = "End Game";
+            this.EndGame.UseVisualStyleBackColor = true;
+            this.EndGame.Click += new System.EventHandler(this.EndGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 733);
+            this.Controls.Add(this.EndGame);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.CurrentSelectedLabel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.MoveLabel);
+            this.Controls.Add(this.StartGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartGame;
+        private System.Windows.Forms.Label MoveLabel;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label CurrentSelectedLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button EndGame;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MindGame
+{
+    public partial class Form1 : Form
+    {
+        public Button[] buttons = new Button[16];
+        public int score = 0;
+        public int[] positions = new int[16];
+        public int moveCount = 0;
+        public int currentSelected;
+        public Button currentSelectedButton;
+        System.Timers.Timer timer = new System.Timers.Timer();
+        System.Timers.Timer startGameTimer = new System.Timers.Timer();
+        Button currentButton;
+        int[] randomNumbers = new int[16];
+
+        public int Score
+        {
+            get { return this.score; }
+            set { this.score = value; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            //this.PlayGame.Enabled = false;
+            timer.Interval = 2000;
+            startGameTimer.Interval = 3000;
+
+            tableLayoutPanel1.ColumnStyles.Clear();
+            for (int i = 0; i < tableLayoutPanel1.ColumnCount; i++)
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+            tableLayoutPanel1.RowStyles.Clear();
+            for (int i = 0; i < tableLayoutPanel1.RowCount; i++)
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+        }
+
+        private void StartGame_Click(object sender, EventArgs e)
+        {
+            randomNumbers = GetRandomNumbers(8);
+            for (var i = 0; i < 16; i++)
+            {
+                buttons[i] = new Button();
+                //if (i <= 7)
+                //{
+                //    buttons[i].Text = (i + 1).ToString();
+                //    positions[i] = i + 1;
+                //}
+                //else
+                //{
+                //    buttons[i].Text = randomNumbers[i - 8].ToString();
+                //    positions[i] = randomNumbers[i - 8];
+                //}
+                buttons[i].Text = randomNumbers[i].ToString();
+                positions[i] = randomNumbers[i];
+
+                buttons[i].Name = i.ToString();
+                buttons[i].Visible = true;
+                buttons[i].Click += PlayMove;
+                buttons[i].Enabled = false;
+                buttons[i].Height = 60;
+                buttons[i].Width = 60;
+                this.tableLayoutPanel1.Controls.Add(buttons[i]);
+            }
+            this.StartGame.Enabled = false;
+            //this.PlayGame.Enabled = true;
+            this.Controls.Add(tableLayoutPanel1);
+
+            // startGameTimer.Stop();
+            startGameTimer.Start();
+            startGameTimer.Elapsed += ResetButtonText;
+        }
+
+        private void PlayMove(object sender, EventArgs e)
+        {
+            currentButton = (Button)sender;
+            string buttonName = ((Button)sender).Name;
+            if (moveCount % 2 == 0)
+            {
+                currentSelectedButton = (Button)sender;
+                //currentSelected = int.Parse(buttonName);
+                currentSelected = positions[int.Parse(buttonName)];
+                currentSelectedButton.Text = currentSelected.ToString();
+                currentSelectedButton.Enabled = false;
+                currentSelectedButton.BackColor = System.Drawing.Color.Yellow;
+            }
+            else
+            {
+                currentButton.Text = positions[int.Parse(currentButton.Name.ToString())].ToString();
+                if (positions[int.Parse(buttonName)] == currentSelected)
+                {
+                    this.score = this.score + 2;
+                    currentSelectedButton.Enabled = false;
+                    ((Button)sender).Enabled = false;
+                    ((Button)sender).Text = currentSelected.ToString();
+                    ((Button)sender).BackColor = System.Drawing.Color.Green;
+                }
+                else
+                {
+                    currentSelectedButton.Enabled = true;
+                    // currentSelectedButton.Text = "";
+                    timer.Stop();
+                    timer.Start();
+                    timer.Elapsed += ClearButtonText;
+                }
+                this.CurrentSelectedLabel.Text = "";
+                currentSelectedButton.BackColor = DefaultBackColor;
+            }
+            moveCount++;
+            this.MoveLabel.Text = "Moves:" + moveCount.ToString();
+            this.ScoreLabel.Text = "Score: " + score.ToString();
+            if (score == 16)
+            {
+                MessageBox.Show("Congratulations you win!! Total Moves: " + moveCount);
+                this.Visible = false;
+            }
+        }
+
+        private static int[] GetRandomNumbers(int n)
+        {
+            //int[] randomNumbersArray = new int[n];
+            //for (int index = 0; index < n;)
+            //{
+            //    int number = random.Next(1, 9);
+            //    if (!randomNumbersArray.Contains(number))
+            //    {
+            //        randomNumbersArray[index] = number;
+            //        index++;
+            //    }
+            //}
+            Random random = new Random();
+            int[] randomNumbers = new int[16];
+            for (int i = 1; i <= 16; i = i + 2)
+            {
+                randomNumbers[i - 1] = i;
+                randomNumbers[i] = i;
+            }
+            int num;
+            int temp;
+            for (int i = 1; i <= 10; i++)
+            {
+                num = random.Next(1, 9);
+                temp = randomNumbers[i - 1];
+                randomNumbers[i - 1] = randomNumbers[num - 1 + 8];
+                randomNumbers[num - 1 + 8] = temp;
+            }
+            return randomNumbers;
+            // return randomNumbersArray;
+        }
+        //private void PlayGame_Click(object sender, EventArgs e)
+        //{
+        //    for (var i = 0; i < 16; i++)
+        //    {
+        //        buttons[i].Text = "";
+        //        buttons[i].Enabled = true;
+        //    }
+        //    this.PlayGame.Enabled = false;
+        //}
+
+        public void ClearButtonText(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            currentButton.Text = "";
+            currentSelectedButton.Text = "";
+            timer.Stop();
+        }
+
+        private void EndGame_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Total Moves: " + moveCount + "  Score: " + score);
+            this.Visible = false;
+        }
+
+        public void ResetButtonText(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            for (var i = 0; i < 16; i++)
+            {
+                buttons[i].Text = "";
+                buttons[i].Enabled = true;
+            }
+            startGameTimer.Stop();
+        }
+
+    }
+}
